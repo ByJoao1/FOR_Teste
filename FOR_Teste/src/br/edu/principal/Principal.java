b862bdb35ff9f2bf4a84d70809c55a2dd490ce8a
João Pedro <141186203+ByJoao1@users.noreply.github.com>
2025-04-08 10:37:42 -0300

    Add files via upload

---
+++ FOR_Teste/src/br/edu/principal/Principal.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		for(int j = 0; j < 10; j ++){
+		System.out.println("IFCE - MPE -"+ j);
+		}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+
+}
